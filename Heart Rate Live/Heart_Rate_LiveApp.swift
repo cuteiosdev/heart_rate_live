2c1ab006e2dd3a643a0fcc5e593f02b1c95d4f6f
Amy Delves <amy@delightfulapps.co>
2023-03-23 09:31:32 +1100

    Initial Commit

---
+++ Heart Rate Live/Heart_Rate_LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heart_Rate_LiveApp.swift
+//  Heart Rate Live
+//
+//  Created by Amy Delves on 23/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Heart_Rate_LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Heart Rate LiveUITests/Heart_Rate_LiveUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Heart_Rate_LiveUITestsLaunchTests.swift
+//  Heart Rate LiveUITests
+//
+//  Created by Amy Delves on 23/3/2023.
+//
+
+import XCTest
+
+final class Heart_Rate_LiveUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
